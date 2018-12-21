aaf1d8a1d687793cb07345441742ef104c9b6dbd
Nikkie Mashian <nikkiemashian@gmail.com>
2018-12-21 09:21:20 -0800

    Add Redux actions, reducer and store

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+
+import cheeseReducer from './reducers/cheese';
+
+export default createStore(cheeseReducer, applyMiddleware(thunk));
+
